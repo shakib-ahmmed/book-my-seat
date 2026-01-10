8e6325a440465635fedeeb9818da91f4ab0c986e
SHAKIB AHMMED <shakibahmmed1@gmail.com>
2026-01-10 23:01:47 +0600

    homepage updated and new sections are added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
-import HeroSection from '../components/Home/HeroSection';
-
-import HomeAdvertisedTickets from '../components/Home/HomeAdvertisedTickets';
-import LatestTickets from '../components/Home/LatestTickets';
-import Categories from '../components/Home/Categories';
-
-
+import React from "react";
+import HeroSection from "../components/Home/HeroSection";
+import HomeAdvertisedTickets from "../components/Home/HomeAdvertisedTickets";
+import LatestTickets from "../components/Home/LatestTickets";
+import Categories from "../components/Home/Categories";
+import PopularRoutes from "../components/Home/PopularRoutes";
+import HowItWorks from "../components/Home/HowItWorks";
+import Testimonials from "../components/Home/Testimonials";
+import Stats from "../components/Home/Stats";
+import Blog from "../components/Home/Blog";
+import Newsletter from "../components/Home/Newsletter";
+import FAQ from "../components/Home/FAQ";
+import CTA from "../components/Home/CTA";
 
 const Home = () => {
     return (
-        <div>
-            <div>
-                <HeroSection />
-            </div>
-
-            <div>
-                <HomeAdvertisedTickets />
-            </div>
-
-            <div>
-                <LatestTickets />
-            </div>
-            <div>
-                <Categories />
-            </div>
+        <div className="bg-white dark:bg-[#1B1B1B] text-black dark:text-white transition-colors duration-500">
+            <HeroSection />
+            <HomeAdvertisedTickets />
+            <LatestTickets />
+            <Categories />
+            <PopularRoutes />
+            <HowItWorks />
+            <Testimonials />
+            <Stats />
+            <Blog />
+            <Newsletter />
+            <FAQ />
+            <CTA />
         </div>
     );
 };
